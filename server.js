6edbbe409e289840efcda080906739f8ee5e7417
monano <mona.gaihab@gmail.com>
2020-05-10 02:07:53 +0300

    test

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express'); // Loading the express module on our server
+const app = express(); // Creates a new instance of express on our server
+const port = 3000
+
+app.get("/", function(req, res){
+  // display 'Hello World!'
+  res.send('Hello World!');
+});
+
+app.listen(3000, function() {
+  // tells the server where to listen for requests on port 3000
+
+  console.log('hello-express is listening on port ' + port);
+}); // actualizing the line above
